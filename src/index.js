07b19b26b95414589674117bdc793f6b0b6d7f15
md-riaj11 <contact.riaj@gmail.com>
2022-12-04 15:35:32 +0600

    clean app.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { routes } from "./Routes/Routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={routes}>
-      <App />
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 );
 
